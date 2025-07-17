97c3edf41a4c7371f007cd76d81d70aa451e80ea
kazuyakurashima <gs.kaz224@gmail.com>
2025-07-17 18:32:40 +0900

    環境変数の設定

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabase =createClient();
+export const supabase =createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_API_KEY,
+);
 // supabaseと繋げるために、urlとapiをここに記入する。
 // 直接ではなく、環境変数で渡す。
